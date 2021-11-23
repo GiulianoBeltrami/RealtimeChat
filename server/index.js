a302ddabfd88d76d0fd5dac717c11e491ae63bf5
Giuliano Beltrami <juka2027@live.com>
2021-11-23 18:02:44 -0300

    Inicio do componente join

--- server/index.js
+++ server/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const router = require('./router');
+
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(router);
 const server = http.createServer(app);
+
+
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log("New socket connection");
+
+    socket.on('disconnect', () => {
+        console.log('Someone has disconencted');
+    })
+})
+
+server.listen(PORT, () =>{
+    console.log(`Server has started on port ${PORT}`)
+});
