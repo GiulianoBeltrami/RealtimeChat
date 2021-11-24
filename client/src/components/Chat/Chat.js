b7e13029e9db0743d65c44e9e6548bae986eceb4
Giuliano Beltrami <juka2027@live.com>
2021-11-24 12:07:09 -0300

    inicio de send message

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -7,6 +7,8 @@ let socket;
 const Chat = () => {
     const [name,setName] = useState('');
     const [room,setRoom] = useState('');
+    const [message,setMessage] = useState('');
+    const [messages,setMessages] = useState([]);
     const ENDPOINT = 'http://localhost:3000/';
     const urlQueryString = window.location.search;
 
@@ -17,18 +19,44 @@ const Chat = () => {
         setName(name);
         setRoom(room);
 
-        socket.emit('join',{ name , room}, () => {
+        socket.emit('join',{name , room}, () => {
 
         });
 
         return () => {
-            socket.emit('disconnect');
-            socket.off();
+            //socket.emit('disconnect');
+            //socket.off();
         }
     }, [ENDPOINT,urlQueryString] );
 
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages,message]);
+        })
+    },[messages])
+
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+
+        if(message){
+            socket.emit('sendMessage', message, () =>{
+                setMessage('');
+            })
+        }
+    }
+
+    console.log(message);
+    console.log(messages);
+
     return(
-        <h1>Chat</h1>
+        <div className="outerContainer">
+            <div className="innerContainer">
+                <input value={message} onChange={event => setMessage(event.target.value)}
+                    onKeyPress={event => event.key==='Enter' ? sendMessage(event) : null}
+                />
+            </div>
+        </div>
     );
 }
 
--- server/index.js
+++ server/index.js
@@ -34,6 +34,14 @@ io.on('connection', (socket) => {
         callback();
     })
 
+    socket.on('sendMessage', (message,callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message',{user:user.name , text:message});
+
+        callback();
+    })
+
     socket.on('disconnect', () => {
         console.log('Someone has disconencted');
     })
