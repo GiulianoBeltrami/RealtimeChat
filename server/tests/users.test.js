0b6e3a414641f1534b4e81c8314f2c54126ec745
Giuliano Beltrami <juka2027@live.com>
2021-11-30 13:13:49 -0300

    Novos testes unitários

--- server/tests/users.test.js
+++ server/tests/users.test.js
@@ -1,10 +1,40 @@
 const users = require('../users');
 
+
 test('Adds a user to users array',() => {
-    user = {id:"1",name:"teste",room:"sala_teste"}
+    user = {id:"1",name:"teste",room:"sala_teste"};
     
     users.addUser(user);
 
     expect(users.users).toEqual(expect.arrayContaining([user]));
 });
 
+
+test('Remove a user from users array', () =>{
+    user = {id:"2",name:"teste2",room:"sala_teste2"};
+    users.users.push(user);
+
+    users.removeUser(user.id);
+
+    expect(users.users).toEqual(expect.not.arrayContaining([user]));
+});
+
+
+test('Getting user information from users array',() => {
+    user = {id:"3",name:"teste3",room:"sala_teste3"};
+    users.users.push(user);
+
+    const userInformation = users.getUser(user.id);
+
+    expect(userInformation).toEqual(user);
+});
+
+
+test('Getting all users in the same room from users array',() =>{
+    user = {id:"4",name:"teste4",room:"sala_teste4"};
+    users.users.push(user);
+
+    const usersInTheRoom = users.getUsersInRoom("sala_teste4");
+
+    expect(usersInTheRoom).toEqual(expect.arrayContaining([user]))
+})
--- server/users.js
+++ server/users.js
@@ -28,3 +28,26 @@ const getUser = (id) => users.find((user) => user.id === id);
 const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
 module.exports = { users,addUser, removeUser, getUser, getUsersInRoom };
+
+class Users{
+
+  users = []
+
+  addUser = ({ id, name, room }) => {
+    name = name.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+  
+    const existingUser = this.users.find((user) => user.room === room && user.name === name);
+  
+    if(!name || !room) return { error: 'Username and room are required.' };
+    if(existingUser) return { error: 'Username is taken.' };
+  
+    const user = { id, name, room };
+  
+    this.users.push(user);
+  
+    return { user };
+  }
+
+}
+
