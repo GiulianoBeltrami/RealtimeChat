47730f2bace201ca1b9fd76effd27c01d04dce65
Giuliano Beltrami <juka2027@live.com>
2021-12-01 11:12:12 -0300

    Inicio da classe de sockets

--- server/index.js
+++ server/index.js
@@ -21,50 +21,120 @@ options={
 const io = socketio(server,options);
 
 io.on('connection', socket => {
-    socket.on('join', ({ name , room}, callback) => {
-        
-        const {user} = users.addUser({id:socket.id,name,room});
+    joinSocket(socket);
 
-        console.log('--------------------------------------------------------------');
-        console.log("New user joined: ");
-        console.log(user);
-        console.log('--------------------------------------------------------------');
+    sendMessageSocket(socket);
 
-        socket.join(user.room);
-
-        socket.emit('message',{ user:'admin',text:`Olá ${user.name}, seja bem vindo a sala ${user.room}`});
-        socket.broadcast.to(user.room).emit('message', {user:'admin',text:`${user.name} entrou!`} );
-        
-
-        io.to(user.room).emit('roomData', {room: user.room, users:users.getUsersInRoom(user.room)});
-
-        callback();
-    })
-
-    socket.on('sendMessage', (message,callback) => {
-        const user = users.getUser(socket.id);
-
-        console.log('--------------------------------------------------------------');
-        console.log('User send message');
-        console.log(user);
-        console.log('Send: ',message);
-        console.log('--------------------------------------------------------------');
-        
-        io.to(user.room).emit('message',{user:user.name , text:message});
-
-        callback();
-    })
-
-    socket.on('disconnect', () => {
-        const user = users.removeUser(socket.id);
-
-        if (user){
-            io.to(user.room).emit('message',{ user:'admin',text:`${user.name} saiu da sala!`});
-            io.to(user.room).emit('roomData',{room:user.room , users:users.getUsersInRoom(user.room)});
-        }
-    })
+    disconnectSocket(socket);
 })
 
 server.listen(PORT, () =>{
     console.log(`Server has started on port ${PORT}`)
 });
+
+function joinSocket(socket){
+    return socket.on('join', ({ name , room}, callback) => {
+        
+            const {user} = users.addUser({id:socket.id,name,room});
+
+            socket.join(user.room);
+
+            socket.emit('message',{ user:'admin',text:`Olá ${user.name}, seja bem vindo a sala ${user.room}`});
+
+            socket.broadcast.to(user.room).emit('message', {user:'admin',text:`${user.name} entrou!`} );
+            
+
+            io.to(user.room).emit('roomData', {room: user.room, users:users.getUsersInRoom(user.room)});
+
+            callback();
+        })
+}
+
+function sendMessageSocket(socket){
+    return  socket.on('sendMessage', (message,callback) => {
+                const user = users.getUser(socket.id);
+
+                console.log('--------------------------------------------------------------');
+                console.log('User send message');
+                console.log(user);
+                console.log('Send: ',message);
+                console.log('--------------------------------------------------------------');
+                
+                io.to(user.room).emit('message',{user:user.name , text:message});
+
+                callback();
+            })
+}
+
+function disconnectSocket(socket){
+    return  socket.on('disconnect', () => {
+                const user = users.removeUser(socket.id);
+
+                if (user){
+                    io.to(user.room).emit('message',{ user:'admin',text:`${user.name} saiu da sala!`});
+                    io.to(user.room).emit('roomData',{room:user.room , users:users.getUsersInRoom(user.room)});
+                }
+            })
+}
+
+class chatSocket{
+
+    constructor(server,options){
+        this.server = server;
+        this.options = options;
+        this.io = socketio(server,options);
+    }
+
+    connectionSocket = () => {
+        this.io.on('connection', socket => {
+
+            joinSocket(socket);
+        
+            sendMessageSocket(socket);
+        
+            disconnectSocket(socket);
+        })
+    }
+    
+    joinSocket = (socket) =>{
+        return socket.on('join', ({ name , room}, callback) => {
+            
+            const {user} = users.addUser({id:socket.id,name,room});
+
+            socket.join(user.room);
+
+            socket.emit('message',{ user:'admin',text:`Olá ${user.name}, seja bem vindo a sala ${user.room}`});
+
+            socket.broadcast.to(user.room).emit('message', {user:'admin',text:`${user.name} entrou!`} );
+            
+
+            io.to(user.room).emit('roomData', {room: user.room, users:users.getUsersInRoom(user.room)});
+
+            callback();
+        })
+    }
+    
+
+    sendMessageSocket = (socket) => {
+        return  socket.on('sendMessage', (message,callback) => {
+            const user = users.getUser(socket.id);
+
+            io.to(user.room).emit('message',{user:user.name , text:message});
+
+            callback();
+        })
+    }
+    
+
+    disconnectSocket = (socket) => {
+        return  socket.on('disconnect', () => {
+            const user = users.removeUser(socket.id);
+
+            if (user){
+                io.to(user.room).emit('message',{ user:'admin',text:`${user.name} saiu da sala!`});
+                io.to(user.room).emit('roomData',{room:user.room , users:users.getUsersInRoom(user.room)});
+            }
+        })
+    }
+
+}
--- server/users.js
+++ server/users.js
@@ -1,33 +1,3 @@
-
-const users = [];
-
-const addUser = ({ id, name, room }) => {
-  name = name.trim().toLowerCase();
-  room = room.trim().toLowerCase();
-
-  const existingUser = users.find((user) => user.room === room && user.name === name);
-
-  if(!name || !room) return { error: 'Username and room are required.' };
-  if(existingUser) return { error: 'Username is taken.' };
-
-  const user = { id, name, room };
-
-  users.push(user);
-
-  return { user };
-}
-
-const removeUser = (id) => {
-  const index = users.findIndex((user) => user.id === id);
-
-  if(index !== -1) return users.splice(index, 1)[0];
-}
-
-const getUser = (id) => users.find((user) => user.id === id);
-
-
-const getUsersInRoom = (room) => users.filter((user) => user.room === room);
-
 class Users{
 
   users = []
@@ -109,6 +79,5 @@ class Users{
 
 }
 
-module.exports={users,addUser, removeUser, getUser, getUsersInRoom}
 module.exports=Users;
 
